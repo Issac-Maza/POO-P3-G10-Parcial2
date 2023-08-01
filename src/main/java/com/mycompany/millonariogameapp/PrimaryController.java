0f895fe6886f7051e8517176724f029178ccdbf7
emjorome <emjorome@espol.edu.ec>
2023-08-01 17:55:05 -0500

    Probando fxml

--- src/main/java/com/mycompany/millonariogameapp/PrimaryController.java
+++ src/main/java/com/mycompany/millonariogameapp/PrimaryController.java
@@ -6,8 +6,6 @@ import javafx.scene.control.Button;
 
 public class PrimaryController {
 
-    @FXML
-    private Button btnbase;
 
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
