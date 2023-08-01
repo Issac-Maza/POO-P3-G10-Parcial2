5281b5249f8b51b2926b880e95bbf0ec8c4ef543
Cliente CONTROL-PC <issamaza@espol.edu.ec>
2023-08-01 11:06:49 -0500

    FX CLASES

--- src/main/java/com/mycompany/millonariogameapp/PrimaryController.java
+++ src/main/java/com/mycompany/millonariogameapp/PrimaryController.java
@@ -2,10 +2,13 @@ package com.mycompany.millonariogameapp;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 public class PrimaryController {
 
     @FXML
+    private Button btnbase;
+
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
