9153ec29e5bdda7e9fcdc063c613e7a1f6fdd54b
emjorome <emjorome@espol.edu.ec>
2023-08-06 14:22:55 -0500

    Avance y creacion de ConfiguracionController

---
+++ src/main/java/com/mycompany/millonariogameapp/ConfiguracionesController.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package com.mycompany.millonariogameapp;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author USUARIO
+ */
+public class ConfiguracionesController{
+
+    @FXML
+    private Button admTermAcad;
+    @FXML
+    private Button AdmMatPar;
+    @FXML
+    private Button AdmPreg;
+    @FXML
+    private Button atras;
+
+    /**
+     * Initializes the controller class.
+     */
+
+    
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.mycompany.millonariogameapp {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.base;
 
     opens com.mycompany.millonariogameapp to javafx.fxml;
     exports com.mycompany.millonariogameapp;
